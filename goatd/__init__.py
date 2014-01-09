e1422e57f347568763bead2f0d05a79b89a4ff93
Louis Taylor <kragniz@gmail.com>
2014-01-09 19:19:33 +0000

    Use config file in main()

--- goatd/__init__.py
+++ goatd/__init__.py
@@ -1,12 +1,12 @@
 from __future__ import print_function
 
 import imp
-import sys
 import os
 from functools import wraps
 
 from .decorators import *
 from .goat import Goat
+from .config import Config
 
 def inject_import(name, filename, inject):
     module = imp.new_module(name)
@@ -23,15 +23,12 @@ class Driver(object):
         self.path = driver_path
 
 def main():
-    assert len(sys.argv) > 2
-    driver_path = sys.argv[1]
+    conf = Config.from_file('goatd-config.json')
 
     goatd = imp.new_module('goatd')
     vars(goatd).update(globals())
-    driver = Driver(driver_path, goatd)
-
-    behaviour_path = sys.argv[2]
+    driver = Driver(conf.driver, goatd)
 
     behaviour = inject_import('behaviour',
-                              behaviour_path,
+                              conf.behaviour,
                               {'goat': Goat(driver)})
