2aa373d4b3f6ebe60f5649b841b5ea7c12b2ba76
Louis Taylor <kragniz@gmail.com>
2014-01-12 00:41:09 +0000

    Add docstrings to Goat

--- goatd/goat.py
+++ goatd/goat.py
@@ -1,8 +1,10 @@
 class Goat(object):
+    '''The goat itself. Most of the work is done by the active driver'''
     def __init__(self, driver):
         self.driver = driver
 
     def __getattr__(self, name):
+        '''Return the requested attribute from the currently loaded driver'''
         func = vars(self.driver.module).get(name)
         if func is None:
             raise AttributeError(
