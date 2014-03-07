58481b4a1acd045f89f10b6ed4f64b436403fafa
Louis Taylor <kragniz@gmail.com>
2014-03-07 14:28:50 +0000

    Add better logging to testing script

--- test/test.sh
+++ test/test.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
+function log() {
+    echo ""
+    echo "        $1"
+    echo
+}
+
 ./goatd-start test/config.yaml &
 goatd_pid=$!
 
-sleep 1
+sleep 2
 
-echo
-echo "        TESTING GET..."
-echo
+log "STARTED GOATD WITH PID=$goatd_pid"
+log "TESTING GET..."
 
 curl -i localhost:2222/heading && echo
 
 curl -i localhost:2222/pony && echo
 
-echo
-echo "        TESTING POST..."
-echo
+log "TESTING POST..."
 
 curl -i -X POST -H "Content-Type: application/json" -d '{"quit": true}' http://localhost:2222
+echo
 
-echo kill $goatd_pid
+sleep 2
+
+if kill $goatd_pid 2> /dev/null; then
+    log "FAILED TO STOP, KILLED GOATD";
+else
+    log "TESTS FINISHED";
+fi
