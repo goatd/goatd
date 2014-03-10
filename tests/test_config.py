dee1a74dee4db2b39c6fa73858d3ac82557e774c
Louis Taylor <kragniz@gmail.com>
2014-03-10 19:50:32 +0000

    Add tests for json config and attributes

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,7 +5,20 @@ class TestConfig(object):
     def setup(self):
         self.directory, _ = os.path.split(__file__)
         self.yaml_file = os.path.join(self.directory, 'config.yaml')
+        self.json_file = os.path.join(self.directory, 'config.json')
 
     def test_load_yaml(self):
         config = goatd.Config.from_yaml(self.yaml_file)
         assert config.goatd
+
+    def test_load_json(self):
+        config = goatd.Config.from_json(self.json_file)
+        assert config.goatd
+
+    def test_port(self):
+        config = goatd.Config.from_yaml(self.yaml_file)
+        assert config.goatd.port == 2222
+
+    def test_driver(self):
+        config = goatd.Config.from_yaml(self.yaml_file)
+        assert config.scripts.driver == 'driver.py'
