a26e568f377535826b3a3340af00c9bf277cc738
Louis Taylor <kragniz@gmail.com>
2014-03-12 14:25:52 +0000

    Test a GET request

--- goatd/tests/test_api.py
+++ goatd/tests/test_api.py
@@ -1,3 +1,8 @@
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+
 import threading
 
 import goatd
@@ -7,12 +12,25 @@ class MockGoat(object):
         self.heading = 45
 
 class TestAPI(object):
+    TEST_PORTS = 10
+    def __init__(self):
+        self.port = 2222
+
     def setup(self):
-        httpd = goatd.GoatdHTTPServer(MockGoat(), ('', 2222),
-                    goatd.GoatdRequestHandler)
+        for _ in range(self.TEST_PORTS):
+            try:
+                httpd = goatd.GoatdHTTPServer(MockGoat(), ('', self.port),
+                            goatd.GoatdRequestHandler)
+                break
+            except Exception, e:
+                self.port += 1
+
         self.http_thread = threading.Thread(target=httpd.handle_request)
         self.http_thread.daemon = True
         self.http_thread.start()
 
     def test_thread(self):
         assert self.http_thread.is_alive()
+
+    def test_GET(self):
+        assert urlopen('http://localhost:2222').read()
