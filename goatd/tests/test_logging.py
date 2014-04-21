204621bee635180ddacd5679014ec386147a459b
Louis Taylor <kragniz@gmail.com>
2014-04-21 03:58:19 +0000

    Clean log directory in tests

--- goatd/tests/test_logging.py
+++ goatd/tests/test_logging.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 import goatd
 
 class MockGoat(object):
@@ -8,9 +11,18 @@ class MockGoat(object):
         self.pos += 1
         return (self.pos, self.pos)
 
-class TestLogging(object):
+class TestGpxLogging(object):
     def setup(self):
         self.goat = MockGoat()
+        self.conf = goatd.Config.from_yaml('goatd-config.yaml')
+
+        log_dir = os.path.dirname(self.conf.goatd.log.gpx.filename)
+        if os.path.isdir(log_dir):
+            #make sure the log directory is clean
+            shutil.rmtree(log_dir)
 
     def test_mock(self):
         assert self.goat.position() == (1, 1)
+
+    def test_correct_config_file(self):
+        assert self.conf.name == 'goatd-test-config'
