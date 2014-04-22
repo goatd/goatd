e89f78300096e736c13925b945dd89d53b83e778
Louis Taylor <kragniz@gmail.com>
2014-04-22 09:34:23 +0100

    Add some simple tests for a clean log environment

--- goatd/tests/test_logging.py
+++ goatd/tests/test_logging.py
@@ -16,13 +16,19 @@ class TestGpxLogging(object):
         self.goat = MockGoat()
         self.conf = goatd.Config.from_yaml('goatd-config.yaml')
 
-        log_dir = os.path.dirname(self.conf.goatd.log.gpx.filename)
-        if os.path.isdir(log_dir):
+        base_name = self.conf.goatd.log.gpx.filename
+        self.log_dir, name = os.path.split(base_name)
+        if os.path.exists(self.log_dir):
             #make sure the log directory is clean
-            shutil.rmtree(log_dir)
+            shutil.rmtree(self.log_dir)
+
+        self.logger = goatd.logging.GpxLogger(self.goat, base_name)
 
     def test_mock(self):
         assert self.goat.position() == (1, 1)
 
     def test_correct_config_file(self):
         assert self.conf.name == 'goatd-test-config'
+
+    def test_dir_created(self):
+        assert os.path.exists(self.log_dir)
