285d36d53174b361600651d435aab733fd70c7ec
Louis Taylor <kragniz@gmail.com>
2014-06-01 21:54:06 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='goatd',
+    version='0.1.0',
+    author='Louis Taylor',
+    author_email='kragniz@gmail.com',
+    description=('Experimental daemon to control an autonomous sailing robot'),
+    license='LGPL',
+    keywords='goat sailing wrapper rest',
+    url='https://github.com/goatd/goatd',
+    packages=['goatd'],
+    scripts=['goatd-start'],
+    requires=['PyYAML'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Embedded Systems',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
