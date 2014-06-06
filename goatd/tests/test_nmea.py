c03f814ba72b1d6810fb969e082d63d9b54848a6
Louis Taylor <kragniz@gmail.com>
2014-06-06 21:30:51 +0100

    Fix negative nmea degrees

--- goatd/tests/test_nmea.py
+++ goatd/tests/test_nmea.py
@@ -5,3 +5,10 @@ def test_hdm():
 
 def test_degrees_to_nmea():
     assert goatd.nmea.degrees_to_nmea(45.555) == '4533.3'
+
+def test_negative_degrees_to_nmea():
+    assert goatd.nmea.degrees_to_nmea(-45.555) == '-4533.3'
+
+'''def test_gll():
+    print goatd.nmea.gll(38.063413, 122.240910, None)
+    assert False'''
