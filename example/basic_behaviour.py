a217b6bdd78558a84917677f3e656a4e4cfbf65d
Louis Taylor <louis@kragniz.eu>
2015-09-03 15:31:11 +0100

    Update the example behaviour to reflect reality

--- example/basic_behaviour.py
+++ example/basic_behaviour.py
@@ -1,9 +1,15 @@
-import time
+# This uses goatd_client, a python library for interacting with goatd. For more
+# information, see https://github.com/goatd/python-goatd
+# Run with $ python basic_behaviour.py, after goatd is running
+
+from goatd_client import Goat
+
+goat = Goat()
 
 for i in range(5):
-    goat.heading()
-    goat.get_wind()
-    goat.position()
+    goat.heading
+    goat.wind
+    goat.position
     goat.rudder(0)
     goat.rudder(-1)
     goat.rudder(3)
