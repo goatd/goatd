3b221b519ff7792ef51b34ca0d3dc935599f614a
Louis Taylor <louis@kragniz.eu>
2015-09-05 19:14:26 +0100

    Move logging module to logger

--- goatd/api.py
+++ goatd/api.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
-from . import logging
+from . import logger
 import json
 
 VERSION = 1.1
@@ -51,7 +51,7 @@ class GoatdHTTPServer(HTTPServer):
             return {'direction': self.goat.wind_direction(),
                     'speed': speed}
         except AttributeError as e:
-            logging.log(e, logging.ERROR)
+            logger.log(e, logger.ERROR)
             raise AttributeError(e)
 
     def goat_active(self):
@@ -145,7 +145,7 @@ class GoatdRequestHandler(BaseHTTPRequestHandler):
         try:
             data = json.loads(post_body)
         except ValueError:
-            logging.log('Can\'t decode {}'.format(post_body), logging.ERROR)
+            logger.log('Can\'t decode {}'.format(post_body), logger.ERROR)
             self.send_json("400 - bad json syntax", 400)
         else:
             response_data = self.server.goat_post_function(self.path, data)
@@ -153,4 +153,4 @@ class GoatdRequestHandler(BaseHTTPRequestHandler):
 
     def log_request(self, code='-', size='-'):
         '''Log the request stdout.'''
-        logging.log('{} requested'.format(self.path), level=logging.VERBOSE)
+        logger.log('{} requested'.format(self.path), level=logger.VERBOSE)
