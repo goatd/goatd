b38ce50540dede2345fbd6f98836ba9303f54c47
Louis Taylor <louis@kragniz.eu>
2016-09-05 10:22:57 +0100

    Change mock goat to use integer

--- goatd/tests/test_goat.py
+++ goatd/tests/test_goat.py
@@ -4,14 +4,14 @@ import goatd
 
 class MockDriver(object):
     def heading(self):
-        return 2.43
+        return 2
 
 class TestGoat(unittest.TestCase):
     def setUp(self):
         self.goat = goatd.Goat(MockDriver())
 
     def test_get_heading(self):
-        assert self.goat.heading() == 2.43
+        assert self.goat.heading() == 2
 
     def test_active(self):
         assert not self.goat.active
