b83122df0a71a386a1d0ded0774205c86d0364b1
Louis Taylor <louis@kragniz.eu>
2016-09-05 10:36:44 +0100

    Update cached sensor values in goat test

--- goatd/tests/test_goat.py
+++ goatd/tests/test_goat.py
@@ -4,14 +4,15 @@ import goatd
 
 class MockDriver(object):
     def heading(self):
-        return 2
+        return 2.43
 
 class TestGoat(unittest.TestCase):
     def setUp(self):
         self.goat = goatd.Goat(MockDriver())
+        self.goat.update_cached_values()
 
     def test_get_heading(self):
-        assert self.goat.heading() == 2
+        assert self.goat.heading() == 2.43
 
     def test_active(self):
         assert not self.goat.active
